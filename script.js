e54af078bed9e8dd2ff2cc475cd60924bf09405c
gilangramadhanhatno <gilangramadhanhatno@gmail.com>
2021-03-29 12:03:36 +0700

    menambahkan fix-nav

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
+// Mobile Nav
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+// Fix Nav
+const navBar = document.querySelector("nav");
+const navHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("cilck", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const el = document.getElementById(id);
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+  });
+});
+
+// GSAP
+// gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+// gsap.from(".menu-toggle", { opacity: 0, duration: 1, delay: 1, x: 20 });
+// gsap.from(".menu", { opacity: 0, duration: 1, delay: 1.5, x: 20 });
+// gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 2, x: 200 });
+// gsap.from(".hero-img-cut", { opacity: 0, duration: 1, delay: 2, x: 200 });
+// gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2.5, y: -50 });
+// gsap.from(".hero-content p", { opacity: 0, duration: 1, delay: 3, y: -45 });
+// gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 4, y: 50 });
